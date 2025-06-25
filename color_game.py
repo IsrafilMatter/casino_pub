9084cf1761a66e712871cfcbc6c850fbac262960
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-06-26 07:32:09 +0800

    color game flow

---
+++ color_game.py
@@ -0,0 +1,16 @@
+# Color Casino Game
+# Israfil Palabay
+# 2025-06-24
+
+# Import necessary libraries
+# Inherit from CasinoGame class
+# Initialization
+# GUI Setup
+# Betting Logic
+# Game Logic
+# Winning Calculation
+# Game state updates
+# Save/Load Game
+# Exit handling
+# Public methods
+# Main entry
