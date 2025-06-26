6bc3ebf40f9fbea83230abfa4c3b3c23af1fa9df
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-06-26 13:17:03 +0800

    add CasinoGame abstract class for game structure and balance management

---
+++ casino_game.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+class CasinoGame(ABC):
+    def __init__(self, player_account_balance):
+        self._player_account_balance = player_account_balance
+
+    @abstractmethod
+    def start_game(self):
+        pass
+
+    @abstractmethod
+    def show_rules(self):
+        pass
+
+    def get_balance(self):
+        return self._player_account_balance
+
+    def set_balance(self, new_balance):
+        self._player_account_balance = new_balance 
